120f5a3f7495acbf0965d0bd5ee96c4ae320b3a2
Lukas Radovansky <radovluk@fel.cvut.cz>
2025-02-18 16:52:46 +0100

    Add attack contract to vault contract

--- contracts/Vault03.sol
+++ contracts/Vault03.sol
@@ -19,3 +19,23 @@ contract Vault03 {
         return true;
     }
 }
+
+interface IVault03 {
+    function breachVault() external returns (bool);
+}
+
+contract Vault03Attack {
+    IVault03 public vault;
+
+    // Constructor to set the address of the vault contract
+    constructor(address _vaultAddress) {
+        vault = IVault03(_vaultAddress);
+    }
+
+    // Function to perform the attack
+    function attack() external returns (bool) {
+        // =========================
+        // YOUR CODE GOES HERE
+        // =========================
+    }
+}
--- contracts/Vault04.sol
+++ contracts/Vault04.sol
@@ -27,3 +27,23 @@ contract Vault04 {
         return true;
     }
 }
+
+interface IVault04 {
+    function breachVault(bytes32 _password) external returns (bool);
+}
+
+contract Vault04Attack {
+    IVault04 public vault;
+
+    // Constructor to set the address of the vault contract
+    constructor(address _vaultAddress) {
+        vault = IVault04(_vaultAddress);
+    }
+
+    // Function to perform the attack
+    function attack() external returns (bool) {
+        // =========================
+        // YOUR CODE GOES HERE
+        // =========================
+    }
+}
